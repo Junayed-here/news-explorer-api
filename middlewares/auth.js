9be1cda7de6bb88cf97047e93cdff012bf07dd42
imjunaid <shopnobazjunaid@gmail.com>
2021-08-05 02:01:29 +0600

    merged

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new Unauthorized('Authorization Required!'));
+    next(new Unauthorized('Authorization Required!++'));
     return;
   }
 
