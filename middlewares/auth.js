24a1061d083808a64287a5ac62e32a36c66fc71e
imjunaid <shopnobazjunaid@gmail.com>
2022-08-13 20:26:33 +0600

    2nd try COR

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,29 +3,6 @@ const Unauthorized = require('../error/Unauthorized');
 
 const { JWT_SECRET, NODE_ENV } = process.env;
 
-// An array of domains from which cross-domain requests are allowed
-const allowedCors = [
-  'https://news.mdjunayed.com',
-  'http://news.mdjunayed.com',
-  'https://api.news.mdjunayed.com',
-  'http://api.news.mdjunayed.com'
-];
-
-app.use(function(req, res, next) {
-  const { origin } = req.headers; // saving the request source to the 'origin' variable
-  // checking that the source of the request is mentioned in the list of allowed ones 
-  if (allowedCors.includes(origin)) {
-    // setting a header that allows the browser to make requests from this source
-    res.header('Access-Control-Allow-Origin', origin);
-
-    // setting a header that allows the browser to make requests from any source
-    // res.header('Access-Control-Allow-Origin', "*");
-
-  }
-
-  next();
-});
-
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
