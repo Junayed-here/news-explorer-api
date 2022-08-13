7b19978062cc5ee04ce6725d866c097c2bb10f82
imjunaid <shopnobazjunaid@gmail.com>
2022-08-13 20:24:11 +0600

    2nd try COR

--- app.js
+++ app.js
@@ -20,29 +20,6 @@ mongoose.connect(DATABASEURL, {
   useUnifiedTopology: true,
 });
 
-// An array of domains from which cross-domain requests are allowed
-const allowedCors = [
-  'https://news.mdjunayed.com',
-  'http://news.mdjunayed.com',
-  'https://api.news.mdjunayed.com',
-  'http://api.news.mdjunayed.com'
-];
-
-app.use(function(req, res, next) {
-  const { origin } = req.headers; // saving the request source to the 'origin' variable
-  // checking that the source of the request is mentioned in the list of allowed ones 
-  if (allowedCors.includes(origin)) {
-    // setting a header that allows the browser to make requests from this source
-    res.header('Access-Control-Allow-Origin', origin);
-
-    // setting a header that allows the browser to make requests from any source
-    // res.header('Access-Control-Allow-Origin', "*");
-
-  }
-
-  next();
-});
-
 app.use(helmet());
 app.disable('x-powered-by');
 app.use(bodyParser.json());
